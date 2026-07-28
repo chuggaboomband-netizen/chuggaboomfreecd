a4f42b6094fbf3265e60dcf37cc2de467a36ba22
Jake Barnes <jakebarnes@Mac.localdomain>
2026-07-28 16:39:37 +0100

    Update Meta pixel ID

--- app/public-meta-pixel.tsx
+++ app/public-meta-pixel.tsx
@@ -44,7 +44,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '351993246460718');
+fbq('init', '860464170281075');
 fbq('track', 'PageView');`}
       </Script>
       <noscript>
@@ -52,7 +52,7 @@ fbq('track', 'PageView');`}
           height="1"
           width="1"
           style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=351993246460718&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=860464170281075&ev=PageView&noscript=1"
           alt=""
         />
       </noscript>
